1dba10b35c8ea8f8c7a3923326e48cfae53d6c16
LeeMin9yeon9 <122258997+LeeMin9yeon9@users.noreply.github.com>
2026-04-17 16:31:38 +0900

    Update SessionController.java

--- springboot_redis/src/main/java/com/example/springboot_redis/SessionController.java
+++ springboot_redis/src/main/java/com/example/springboot_redis/SessionController.java
@@ -10,13 +10,17 @@ public class SessionController {
     @Value("${server.port:8080}")
     private String port;
 
+    @Value("${HOSTNAME:unknown}")
+    private String hostname;
+
     @GetMapping("/")
     public String index(HttpSession session) {
-        if (session.getAttribute("visit-port") == null) {
-            session.setAttribute("visit-port", port);
+        if (session.getAttribute("visit-server") == null) {
+            session.setAttribute("visit-server", hostname);
         }
+
         return "<h3>Session Clustering Test</h3>" +
-               "현재 응답 중인 서버 포트: " + port + "<br>" +
-               "세션에 기록된 최초 접속 서버 포트: " + session.getAttribute("visit-port");
+               "현재 응답 중인 서버: " + hostname + "<br>" +
+               "세션에 기록된 최초 접속 서버: " + session.getAttribute("visit-server");
     }
 }
